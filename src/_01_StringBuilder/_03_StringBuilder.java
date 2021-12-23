82add096265b1c4695286d20f56f4eef106fef8c
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2021-12-22 17:28:12 -0800

    Work on the wave

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -1,25 +1,31 @@
 package _01_StringBuilder;
 
-
 public class _03_StringBuilder {
-    
-    public static String append(String str, char[] characters) {
-        return null;
-    }
-    
-    public static String reverse(String str) {
-      
-    	System.out.println(str.reverse().toString());
-    	str.reverse();
-    	
-    	return str;
-    }
-    
-    public static String insert(String str, int index, char newChar) {
-        return null;
-    }
-    
-    public static String delete(String str, int startIndex, int endIndex) {
-        return null;
-    }
+
+	public static String append(String str, char[] characters) {
+
+		StringBuilder builder = new StringBuilder(str);
+
+		return builder.append(characters).toString();
+
+	}
+
+	public static String reverse(String str) {
+
+		StringBuilder builder = new StringBuilder(str);
+
+		return builder.reverse().toString();
+	}
+
+	public static String insert(String str, int index, char newChar) {
+		StringBuilder builder = new StringBuilder(str);
+
+		return builder.insert(index, newChar).toString();
+	}
+
+	public static String delete(String str, int startIndex, int endIndex) {
+		StringBuilder builder = new StringBuilder(str);
+		
+		return builder.delete(startIndex, endIndex).toString();
+	}
 }
